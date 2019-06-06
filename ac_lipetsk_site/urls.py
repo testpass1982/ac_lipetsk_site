fa1238f61ffa103d79166e71ea4a9a11e950629d
Anatoliy Popov <popov.anatoly@gmail.com>
2019-06-06 16:59:12 +0300

    initial

---
+++ ac_lipetsk_site/urls.py
@@ -0,0 +1,44 @@
+"""ac_lipetsk_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, re_path, include
+import mainapp.views as mainapp
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', mainapp.index, name='index'),
+    path('about/', mainapp.about, name='about'),
+    path('contact/', mainapp.contact, name='contact'),
+    path('staff/', mainapp.staff, name='staff'),
+    path('documents/', mainapp.documents, name='documents'),
+    path('news/', mainapp.news, name='news'),
+    path('detailview/<slug:content>/<slug:pk>',
+         mainapp.details, name='detailview'),
+    path('create/<slug:content_type>', mainapp.create_factory, name='create'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('messages/', mainapp.messages, name='messages'),
+    path('service/<slug:pk>', mainapp.services, name="service"),
+    path('reestrsp/', mainapp.reestrsp, name='reestrsp'),
+    path('import_profile/', mainapp.import_profile, name='import_proflie'),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ static/js/myscript.js
@@ -0,0 +1,95 @@
+//Включение всплывающих подсказок
+$(function () {
+	$('[data-toggle="popover"]').popover();
+	$('[data-toggle="tooltip"]').tooltip();
+})
+
+// Кнопка - Наверх
+$(document).ready(function() {
+	$('body').append('<button class="top-arrow">');
+	$('.top-arrow').click(function() {
+		$('body').animate({'scrollTop': 0}, 1000);
+		$('html').animate({'scrollTop': 0}, 1000);
+	});
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > 300) {
+			$(".top-arrow").addClass("top-arrow-active");
+		}
+		else {
+			$(".top-arrow").removeClass("top-arrow-active");
+		}
+	});
+});
+
+// Slider
+$(document).ready(function() {
+	// $("#slider-photo").owlCarousel({
+	// 	margin:10,
+	// 	responsiveClass:true,
+	// 	responsive:{
+	// 		0:{
+	// 			items:1,
+	// 			nav:true
+	// 		},
+	// 		600:{
+	// 			items:3,
+	// 			nav:true
+	// 		},
+	// 		1000:{
+	// 			items:3,
+	// 			nav:true,
+	// 			loop:false
+	// 		}
+	// 	},
+
+	// 	navText: [
+	// 		'<span aria-label="' + 'Previous' + '">&larr;</span>',
+	// 		'<span aria-label="' + 'Next' + '">&rarr;</span>'
+	// 	],
+	// 	// autoPlay:true,
+	// 	nav:true,
+	// 	autoHeight: true,
+	// 	dots:false
+	// });
+
+	// $("#slider-text").owlCarousel({
+	// 	margin:40,
+	// 	responsiveClass:true,
+	// 	responsive:{
+	// 		0:{
+	// 			items:1,
+	// 			nav:true
+	// 		},
+	// 		600:{
+	// 			margin:20,
+	// 			items:3,
+	// 			nav:true
+	// 		},
+	// 		1200:{
+	// 			items:4,
+	// 			nav:true,
+	// 			loop:false
+	// 		}
+	// 	},
+
+	// 	navText: [
+	// 		'<span aria-label="' + 'Previous' + '">&larr;</span>',
+	// 		'<span aria-label="' + 'Next' + '">&rarr;</span>'
+	// 	],
+	// 	// autoPlay:true,
+	// 	nav:true,
+	// 	// autoHeight: true,
+	// 	dots:false
+	// });
+	$('#toggle_panel').click(function(event){
+		event.preventDefault();
+		$('.admin_panel').toggle();
+		if ($('.admin_panel').is(":visible")) {
+		  $('#toggle_panel').html('<i class="fas fa-arrow-up"></i>');
+		} else {
+		  $('#toggle_panel').html('<i class="fas fa-arrow-down"></i>');
+		}
+	  });
+});
+
+
